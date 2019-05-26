e9bf6c383e8df45538f2fe97456c525ab95797b4
Alejandro Dorantes <doranteks@gmail.com>
2019-05-25 20:28:39 -0500

    local

---
+++ src/index.ts
@@ -0,0 +1,130 @@
+import "./normalize.scss";
+import "./custom.scss";
+import "./fonts.scss";
+
+document.getElementById("app").innerHTML = `
+<header>
+<ul>
+<li>🌎 NETWORKS:</li>
+<li>🏀 <a href="https://dribbble.com/alejandro">DRIBBBLE</a></li>
+<li>💼 <a href="https://www.behance.net/adeoh">BEHANCE</a></li>
+<li>✨ <a href="https://are.na/alejandro-dorantes">ARE.NA</a></li>
+</ul>
+<ul>
+<li>📑 <a href="/curriculum">CURRICULUM</a></li>
+<li>🖼 <a href="/portfolio">REQUEST PORTFOLIO</a></li>
+</ul>
+</header>
+
+<article>
+<ul>
+<li>Alejandro Dorantes </li>
+<li>DESIGN, ART & DEVELOPMENT </li>
+<li>Based in SPGG, N.L.M.X.</li>
+</ul>
+</article>
+
+
+<article>
+
+<ul>
+<li>Alejandro Dorantes </li>
+<li>DESIGN, ART & DEVELOPMENT </li>
+<li>Based in SPGG, N.L.M.X.</li>
+</ul>
+
+<ul>
+<li>FOUNDING: </li>
+<li><a href="http://goodies.design">A DESIGN COMMUNITY FOR NOVICES</a></li>
+<li><a href="https://www.facebook.com/plantalibre.mx/">AN EXPERIMENTAL ART GALLERY</a></li>
+<li><a href="http://almanegra.cc">A CULTURE FORMING COLLECTIVE</a></li>
+</ul>
+
+
+<ul>
+<li>LIVE PROJECTS:</li>
+<li><a href="https://pulsar.media/">PULSARⓇ</a> (BRAND/WEB)</li>
+<li><a href="https://ohanapoke.mx">OHANA POKÉ</a> (BRAND/INTERIORS)</li>
+<li><a href="https://www.youtube.com/watch?v=OzwjfE4PqW4">INSITE MUSIC</a>(FILM/TYPE)</li>
+<li><a href="https://dominasistemi.it/">DOMINA SISTEMI</a> (BRANDING)</li>
+
+<li><a href="https://www.infamuse.com">INFAMUSE</a> (INT/UX/UI/DEV)</li>
+<li><a href="http://vira.com.mx">VIRA</a> (INT/UX/UI/DEV)</li>
+<li><a href="https://www.blackpalmdevelopment.com">BLACKPALM DEV.</a> (UX/DEV)</li>
+<li><a href="https://tequiladelaluz.com">DELALUZ</a> (INT/UI/UX/DEV)</li>
+<li><a href="https://panio.mx">PANIO</a> (UX/DEV)</li>
+<li><a href="https://www.arcusfi.com">ARCUS</a> (INT/UI/UX/DEV)</li>
+</ul>
+
+<ul>
+<li>Experience:
+<li><a href="https://anagrama.com">Anagrama Studio</a> (2018)</li>
+<li><a href="https://pulsar.media">Pulsar Mediahouse</a> (2015)</li>
+<li><a href="http://www.productotaller.com/">Producto Taller</a> (2012)</li>
+</ul>
+
+<ul>
+<li>CAPABILITIES:</li>
+<li>DIRECTION</li>
+<li>BRANDING</li>
+<li>HCD DESIGN</li>
+<li>IA/UX/UI</li>
+<li>COPYWRITING</li>
+<li>STRATEGY</li>
+<li>GROWTH HACKING</li>
+<li>INBOUND MARKETING</li>
+<li>AAARRR!</li>
+</ul>
+<ul>
+<li>ASPIRATIONS:</li>
+<li>INTERIOR DESIGN</li>
+<li>BRANDING NETWORKS</li>
+<li>FILM TITLES</li>
+</ul>
+<ul>
+<li>JOURNAL:</li>
+<li><a href="">RANDOM NOTES</a> / 25 MAY 19</li>
+<li><a href="">THOUGHTS ON DESIGN</a> / 20 may 19</li>
+</ul>
+<ul>
+<li>READING:</li>
+<li><a href="">ANTIFRAGILE</a> / N. N. TALEB</li>
+<li><a href="">GRID SYSTEMS</a> / KIMBERLY LEAN</li>
+</ul>
+<ul>
+<li>LISTENING:</li>
+<li>KEVIN ABSTRACT / <a href="">ARIZONA BABY</a></li>
+</ul>
+</article>
+
+<article>
+<ul>
+<li><img src="https://d2w9rnfcy7mm78.cloudfront.net/4269279/large_bbea5739c194b95a5540af69520175eb.png?1557817965"></li>
+<li><img src="https://d2w9rnfcy7mm78.cloudfront.net/4261428/large_ed8f9c5f12f1795c9e9ce6d735fdcea3.jpg?1557729471"></li>
+<li><img src="https://d2w9rnfcy7mm78.cloudfront.net/4261427/large_13e4b06ff1153d7ed02602afeff260dc.png?1557729466"></li>
+
+<li><img src="https://d2w9rnfcy7mm78.cloudfront.net/4337540/large_88ced25da58201bcdd6185562c783c90.png?1558827985"></li>
+
+
+<li><img src="https://d2w9rnfcy7mm78.cloudfront.net/4261753/large_fa9ee5410a649b7bf45744d7a86932c9.png?1557731622"></li>
+<li><img src="https://d2w9rnfcy7mm78.cloudfront.net/4261753/large_fa9ee5410a649b7bf45744d7a86932c9.png?1557731622"></li>
+<li><img src="https://d2w9rnfcy7mm78.cloudfront.net/4261753/large_fa9ee5410a649b7bf45744d7a86932c9.png?1557731622"></li>
+</ul>
+</article>
+
+<footer>
+<ul>
+<li>STATIONS: 📡</li>
+<li><a href="https://unsplash.com/@adeoh">Unsplash</a> 💦</li>
+<li><a href="">YOUTUBE</a> 📺</li>
+<li><a href="">LAST.FM</a> 📻</li>
+
+</ul>
+<ul>
+<li>SOCIAL 🤝</li>
+<li>‍<a href="https://facebook.com/adeohcore">FACEBOOK</a> 🙂</li>
+<li><a href="https://instagram.com/adeoh">INSTAGRAM</a> 📷</li>
+</ul>
+</footer>
+
+`;
